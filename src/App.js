1643e1c48e7b5606c700008892c03edab55ab18f
Mehedi Hasan <mehedi101@outlook.com>
2020-11-28 17:23:54 +0600

    Added clarify API

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Logo from './components/logo/logo';
 import Navigation from './components/navigation/Navigation';
 import Rank from './components/rank/Rank';
 import { particleOpt } from './ParticleOpt';
+import Clarifai from 'clarifai';
 
+const app = new Clarifai.App({
 
+  apiKey: '8c7957e5446740c89b6114f71d5c8c87'
+ });
 
 
 
@@ -19,11 +23,33 @@ class App extends Component {
     this.state = { input:'' }
   }
 
+
   onInputChange = (event) =>{
 
     this.setState({input: event.target.value})
     console.log(event.target.value);
   }
+
+  onBtnSubmit= (e) =>{
+    e.preventDefault();
+
+    console.log('clicked');
+
+
+    app.models.predict(
+      'd02b4508df58432fbb84e800597b8959', 'https://opinion.bdnews24.com/wp-content/uploads/2017/07/bangabandhu111-1170x660.jpg'
+    )
+    .then((response) =>{
+        console.log(response);
+
+    }).then((error)=>{
+        console.log(error);
+
+    });
+
+
+  }
+
 
 
   render() { 
@@ -34,7 +60,10 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm inputChange={this.onInputChange} />
+        <ImageLinkForm 
+        inputChange={this.onInputChange} 
+        btnSubmit={this.onBtnSubmit} 
+        />
 
       </div>
       
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,22 +1,22 @@
 import React from 'react';
-const ImageLinkForm = ( { inputChange } ) => {
+const ImageLinkForm = ( { inputChange, btnSubmit } ) => {
     return ( 
         <div className="tc">
             <p className='f3'>
                 {'This Magic Brain will detect face in an image. Give a try'}
             </p>
-            <form className="pa2 flex justify-center"> 
+            <form className="pa2 flex justify-center" >  
                 <div className="pa4 honeyCombPattern shadow-4">
                     
                   <input  onChange= { inputChange }
                   type="url" 
                   name="imgUrl" 
                   className="pa3"
-                  required
+                 
 
                 /> 
 
-                  <button type="submit"  className="pa3 bg-purple white grow dib ">Detect</button>
+                  <button type="button"  className="pa3 bg-purple white grow dib " onClick={ btnSubmit } >Detect</button>
                 </div>
                
             </form>
